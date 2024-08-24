466dbbd1d51775387e2aae03305d37fd588f4b0d
Maxime ELIAZORD <mzozord@gmail.com>
2024-08-24 13:46:46 +0200

    Added connect options to Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,23 @@
+"use client"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { useConnect, useDisconnect } from "wagmi"
 import { comfortaa } from "@/lib/fonts"
 import { LiaLemon } from "react-icons/lia"
+import useHasMounted from "@/lib/hooks/useHasMounted"
+import { Button } from "./ui/button"
+import { useUser } from "@/lib/hooks/useUser"
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 bg-amber-500">
       <Logo />
+      <Connectors />
     </header>
   )
 }
@@ -16,7 +29,52 @@ function Logo() {
     >
       <span>Lem</span>
       <LiaLemon />
-      <span>nads</span>
+      <span>nAds</span>
+    </div>
+  )
+}
+
+function Connectors() {
+  const { connect, connectors } = useConnect()
+  const { disconnect } = useDisconnect()
+  const { user } = useUser()
+
+  function checkUser() {
+    console.log("user: ", user)
+  }
+
+  const hasMounted = useHasMounted()
+
+  if (!hasMounted) return <span>Not mounted</span>
+
+  return (
+    <div>
+      <DropdownMenu>
+        {user ? (
+          <div className="flex items-center gap-2">
+            <Button onClick={() => disconnect()}>Disconnect</Button>
+            <Button onClick={checkUser}>Info</Button>
+          </div>
+        ) : (
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary">Connect</Button>
+          </DropdownMenuTrigger>
+        )}
+        <DropdownMenuContent>
+          {connectors.map((connector) => {
+            return (
+              <DropdownMenuItem key={connector.id}>
+                <Button
+                  variant="secondary"
+                  onClick={() => connect({ connector })}
+                >
+                  {connector.name}
+                </Button>
+              </DropdownMenuItem>
+            )
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   )
 }
